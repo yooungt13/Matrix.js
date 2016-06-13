995a8a72c331c38e0489f56bb08bc409ab077bac
yooungt13 <61123072@qq.com>
2016-06-13 11:41:36 +0800

    WWF: 初始化构建/运行脚本

---
+++ bin/build.sh
@@ -0,0 +1,13 @@
+# author: youngtian13
+# date: 20160613
+
+# npm install
+#
+# 1.构建
+# gulp/grunt build
+#
+# 2.更新资源
+# update CDN/static server
+#
+# 3.发布代码
+# cp2server
---
+++ bin/run.sh
@@ -0,0 +1,6 @@
+# author: youngtian13
+# date: 20160613
+
+# 检查环境
+# npm install
+npm run prod
