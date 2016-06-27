f3e1a8f54c2b78f325132e95240a6ceb11a01bfa
yooungt13 <61123072@qq.com>
2016-06-27 18:04:58 +0800

    ESLINT: 根据eslint修改pagejs

--- client/resource/build/js/page/main.js
+++ client/resource/build/js/page/main.js
@@ -2,6 +2,7 @@
 var page = require('./module');
 
 page.init();
+
 },{"./module":2}],2:[function(require,module,exports){
 // 通过 require 引入依赖
 // var $ = require('../lib/zepto');
@@ -13,5 +14,6 @@ function init() {
 // 或者通过 module.exports 提供整个接口
 module.exports = {
     init: init
-}
+};
+
 },{}]},{},[1]);
