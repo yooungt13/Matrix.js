da91f2c43238ad66f5cec2327d57bb32312b646f
yooungt13 <61123072@qq.com>
2016-08-19 17:05:49 +0800

    线上缓存模板

--- server/recipe/template.js
+++ server/recipe/template.js
@@ -10,20 +10,22 @@ const render = require('koa-swig');
 module.exports = (app) => {
     const VIEW_PATH = app.config.path.view;
     const DEBUG = app.debug;
+    const ENV = process.env.NODE_ENV || 'dev';
 
     const filters = require(app.config.path.extension + '/filter');
+    const isProd = !!(ENV === 'prod');
 
     // 全局参数，每次调用this.render都会返回
     const GLOBAL = {
-        ENV: process.env.NODE_ENV || 'dev',
+        ENV: ENV,
         STATIC_HOST: app.config.host.static
     };
 
     // 配置template engine
     app.context.render = render({
         root: VIEW_PATH,
-        filters: filters,  // 扩展filter
-        cache: false,      // 禁止模板缓存
+        filters: filters, // 扩展filter
+        cache: isProd, // 禁止模板缓存
         locals: {
             GLOBAL: GLOBAL
         }
